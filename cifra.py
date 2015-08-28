3b9ba615c603d0ea621ecba77b76c8517cba3fac
Henrique Lopes <riquellopes@gmail.com>
2015-08-28 00:52:34 -0300

    Flake8 no arquivo cifra

--- cifra.py
+++ cifra.py
@@ -1,72 +1,74 @@
-# -*- encoding: utf-8 -*-
+# encoding: utf-8
 import string
 
+
 class Cifra(object):
-	
-	def _grid(self):
-		"""
-			Cria grelha de Vigenére.
-		"""
-		grid = {}
-		alphabet = string.ascii_lowercase
-		for pos in xrange(26):
-			letter_now = alphabet[pos]
-			try:
-				letter_previous = alphabet[ pos-1 ]
-				grid[ letter_now ] = grid[letter_previous][1:] + letter_previous
-			except:
-				grid[ letter_now ] = alphabet
-		return grid
-	grid = property(fget=_grid)
-	
-	def crypt(self, text, key, decrypt=False):
-		"""
-			Criptografa texto e decriptografa.
-		"""
-		if len(text) < len(key):
-			raise ValueError("O tamanho da chave deve ser menor do que o do texto.")
-		
-		key = self.match_size(text, key)
-		crypt = ""
-		for l in range( len( text ) ):
-			crypt += self.translater(text[l], key[l], decrypt)
-		return crypt
-	
-	def translater(self, letter, alphabet, decrypt=False):
-		"""
-			Traduz letra usando a grelha de Vigenère.
-		"""
-		try:
-			for pos in xrange(26):
-				if decrypt:
-					index = 'a'
-					alpha = alphabet.lower()
-			 	else:
-					index = alphabet.lower()
-					alpha = 'a'
 
-				if self.grid[alpha][pos] == letter.lower():
-					translater = self.grid[index][pos]
-					break
-			return translater
-		except:
-			return ""
-	
-	def match_size(self, text, key):
-		"""
-			Normaliza tamanho de uma chave.
-		"""
-		textlen = len( text )
-		keylen = len( key )
-		pos_now = 0
-		new_key = ""
-		start = True
+    @property
+    def grid(self):
+        """
+            Cria grelha de Vigenére.
+        """
+        grid = {}
+        alphabet = string.ascii_lowercase
+        for pos in xrange(26):
+            letter_now = alphabet[pos]
+            try:
+                letter_previous = alphabet[pos-1]
+                grid[letter_now] = grid[letter_previous][1:] + letter_previous
+            except:
+                grid[letter_now] = alphabet
+        return grid
 
-		while start:
-			for pos in xrange( keylen ):
-				new_key += key[pos]
-				pos_now += 1
-				if pos_now == textlen:
-					start = False
-					break
-		return new_key
+    def crypt(self, text, key, decrypt=False):
+        """
+            Criptografa texto e decriptografa.
+        """
+        if len(text) < len(key):
+            raise ValueError(
+                "O tamanho da chave deve ser menor do que o do texto.")
+
+        key = self.match_size(text, key)
+        crypt = ""
+        for l in range(len(text)):
+            crypt += self.translater(text[l], key[l], decrypt)
+        return crypt
+
+    def translater(self, letter, alphabet, decrypt=False):
+        """
+            Traduz letra usando a grelha de Vigenère.
+        """
+        try:
+            for pos in xrange(26):
+                if decrypt:
+                    index = 'a'
+                    alpha = alphabet.lower()
+                else:
+                    index = alphabet.lower()
+                    alpha = 'a'
+
+                if self.grid[alpha][pos] == letter.lower():
+                    translater = self.grid[index][pos]
+                    break
+            return translater
+        except:
+            return ""
+
+    def match_size(self, text, key):
+        """
+            Normaliza tamanho de uma chave.
+        """
+        textlen = len(text)
+        keylen = len(key)
+        pos_now = 0
+        new_key = ""
+        start = True
+
+        while start:
+            for pos in xrange(keylen):
+                new_key += key[pos]
+                pos_now += 1
+                if pos_now == textlen:
+                    start = False
+                    break
+        return new_key
--- test_cifra.py
+++ test_cifra.py
@@ -1,24 +1,25 @@
 # -*- encoding: utf-8 -*-
 import unittest
 from nose.tools import assert_raises
-from cifra import Cifra
+from cifra2 import Cifra
+
 
 class TestCifra(unittest.TestCase):
-	
+
 	def test_alfabeto_A(self):
 		"""
 			Testa alfabeto A
 		"""
 		c = Cifra()
 		self.assertEquals(str(c.grid['a']), 'abcdefghijklmnopqrstuvwxyz')
-	
+
 	def test_alfabeto_B(self):
 		"""
 			Testa alfabeto B
 		"""
 		c = Cifra()
 		self.assertEquals(str(c.grid['b']), 'bcdefghijklmnopqrstuvwxyza')
-	
+
 	def test_alfabeto_C(self):
 		"""
 			Testa alfabeto C
@@ -32,28 +33,28 @@ class TestCifra(unittest.TestCase):
 		"""
 		c = Cifra()
 		self.assertEquals(c.translater('A', 'A'), 'a')
-	
+
 	def test_letra_A_no_alfabeto_B_deve_retorno_B(self):
 		"""
 			A letra A na grelha de vigenére deve retornar B no alfabeto B
 		"""
 		c = Cifra()
 		self.assertEquals(c.translater('A', 'B'), 'b')
-	
+
 	def test_letra_A_no_alfabeto_C_deve_retorno_C(self):
 		"""
 			A letra A na grelha de vigenére deve retornar C no alfabeto C
 		"""
 		c = Cifra()
 		self.assertEquals(c.translater('A', 'C'), 'c')
-	
+
 	def test_letra_Z_no_alfabeto_I_deve_retorno_H(self):
 		"""
 			A letra Z na grelha de vigenére deve retornar H no alfabeto I
 		"""
 		c = Cifra()
 		self.assertEquals(c.translater('Z', 'I'), 'h')
-	
+
 	def test_retornar_None_para_o_que_nao_estiver_traduzido(self):
 		"""
 			Para o que não estiver no grelha de Vigenère retorna None.
@@ -67,35 +68,35 @@ class TestCifra(unittest.TestCase):
 		"""
 		c = Cifra()
 		assert_raises(ValueError, c.crypt, 'a', 'bb')
-	
+
 	def test_normaliza_tamanho_LIMAO(self):
 		"""
 			Caso key seja maior que o texto função normaliza tamanho.
 		"""
 		c = Cifra()
 		self.assertEquals(c.match_size('ATACARBASESUL', 'LIMAO'), 'LIMAOLIMAOLIM')
-	
+
 	def test_normaliza_tamanho_LIMA(self):
 		"""
 			A chave LIMA com um texto de 13 caracteres o retorno deve ser LIMALIMALIMAL
-		"""	
+		"""
 		c = Cifra()
 		self.assertEquals(c.match_size('ATACARBASESUL', 'LIMA'), 'LIMALIMALIMAL' )
-	
+
 	def test_ATACARBASESUL_chave_LIMAO_retorno_LBMCOCJMSSDCX(self):
 		"""
 			A criptografia de ATACARBASESUL com a chave LIMAO deve ser LBMCOCJMSSDCX
 		"""
 		c = Cifra()
 		self.assertEquals(c.crypt('ATACARBASESUL', 'LIMAO').upper(), 'LBMCOCJMSSDCX')
-	
+
 	def test_MORREU_chave_UVA_retorno_GJRLZU(self):
 		"""
 			A criptografia de MORREU com a chave UVA deve ser GJRLZU
 		"""
 		c = Cifra()
 		self.assertEquals(c.crypt('MORREU', 'UVA').upper(), 'GJRLZU')
-	
+
 	def test_GJRLZU_chave_UVA_MORREU(self):
 		"""
 			O texto criptografado GJRLZU com chave UVA retorna MORREU
